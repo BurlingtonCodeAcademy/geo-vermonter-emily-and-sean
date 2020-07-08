2c52db2face1a0136eddfa9fa810828665818057
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-07-08 16:29:42 -0400

    Working button functionality.

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,5 +1,46 @@
 import React from "react"
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
+//Creates a class of buttons. Allows use of props 
 class Button extends React.Component{
-    
+    constructor(props){
+        super(props)
+        this.state ={
+            startEnabled: false,
+            guessDisabled: true, 
+            quitDisabled: true 
+        }
+    }
+//When user clicks on start, the start button is disabled and the guess and quit button are enabled
+    handleStart= (evt)=>{
+        
+        evt.preventDefault();
+        this.setState({
+            startEnabled: true,
+            guessDisabled: false,
+            quitDisabled: false
+        })
+        //Declare Variables 
+        let startButton = document.getElementById("startButton");
+        let guessButton = document.getElementById("guessButton");
+        let quitButton = document.getElementById("quitButton");
+
+        startButton.disabled =true;
+        guessButton.disabled =false;
+        quitButton.disabled= false;
+
+       
+    };
+//Renders the page. Creates buttons 
+    render(){
+        return ( 
+       <div>
+           <button class="button" id="startButton" onClick= {this.handleStart}> Start Game</button> {/* Create Start Button */}
+           <button disabled={true} class="button" id= "guessButton" onClick= {this.handleGuess}> Guess</button> {/* Create Guess Button */}
+           <button disabled={true} class="button" id= "quitButton" onClick= {this.handleQuit}> Quit</button> {/* Create Quit Button */}
+       </div> 
+    )}
 }
+
+//Export the file
+export default Button
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import VTMap from './VTMap'
+import Button from "./Components/Buttons.js"
 
 ReactDOM.render(
-  
-    <VTMap />,
+  <>
+    <VTMap />
+    <Button />
+    </>,
     document.getElementById('root')
+    
+    
 )
+
