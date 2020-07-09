1edca0815f4672eb87d94099331352751ff50b5f
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-07-09 14:24:59 -0400

    Random Lat and Lng Generated

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -3,41 +3,15 @@ import React from "react"
 
 //Creates a class of buttons. Allows use of props 
 class Button extends React.Component{
-    constructor(props){
-        super(props)
-        this.state ={
-            startEnabled: false,
-            guessDisabled: true, 
-            quitDisabled: true 
-        }
-    }
-//When user clicks on start, the start button is disabled and the guess and quit button are enabled
-    handleStart= (evt)=>{
-        
-        evt.preventDefault();
-        this.setState({
-            startEnabled: true,
-            guessDisabled: false,
-            quitDisabled: false
-        })
-        //Declare Variables 
-        let startButton = document.getElementById("startButton");
-        let guessButton = document.getElementById("guessButton");
-        let quitButton = document.getElementById("quitButton");
 
-        startButton.disabled =true;
-        guessButton.disabled =false;
-        quitButton.disabled= false;
 
-       
-    };
 //Renders the page. Creates buttons 
     render(){
         return ( 
        <div>
-           <button className="button" id="startButton" onClick= {this.handleStart}> Start Game</button> {/* Create Start Button */}
-           <button disabled={true} className="button" id= "guessButton" onClick= {this.handleGuess}> Guess</button> {/* Create Guess Button */}
-           <button disabled={true} className="button" id= "quitButton" onClick= {this.handleQuit}> Quit</button> {/* Create Quit Button */}
+           <button className="button" id="startButton" onClick= {this.props.handleStart}> Start Game</button> {/* Create Start Button */}
+           <button disabled={true} className="button" id= "guessButton" onClick= {this.props.handleGuess}> Guess</button> {/* Create Guess Button */}
+           <button disabled={true} className="button" id= "quitButton" onClick= {this.props.handleQuit}> Quit</button> {/* Create Quit Button */}
        </div> 
     )}
 }
--- src/index.js
+++ src/index.js
@@ -15,16 +15,49 @@ class App extends React.Component {
       currentLat: [],
       currentLng: [],
       town: '',
-      county: ''
+      county: '',
+      startEnabled: false,
+      guessDisabled: true, 
+      quitDisabled: true 
     }
   }
+
+  //When user clicks on start, the start button is disabled and the guess and quit button are enabled
+  handleStart= (evt)=>{
+        
+    evt.preventDefault();
+    this.setState({
+        startEnabled: true,
+        guessDisabled: false,
+        quitDisabled: false
+    })
+    //Declare Variables 
+    let startButton = document.getElementById("startButton");
+    let guessButton = document.getElementById("guessButton");
+    let quitButton = document.getElementById("quitButton");
+
+    startButton.disabled =true;
+    guessButton.disabled =false;
+    quitButton.disabled= false;
+
+  //Gives a random lat and lng between the max and min lat and lng of Vermont. 
+   let randomLng=  -(Math.random() * (73.40975057247658 - 71.51752027568435 ) +71.51752027568435 )
+   let randomLat =(Math.random()*(45.007561302382754-42.730315121762715  )+42.730315121762715 );
+ 
+ 
+
+   
+};
+
 
   render() {
     console.log(this.state)
     return (
     <>
      <VTMap currentLat={this.state.currentLat} currentLng={this.state.currentLng}/>
-     <Button />
+
+     <Button handleStart= {this.handleStart} startEnabled ={this.startEnabled} guessDisabled= {this.guessDisabled}quitDisabled= {this.quitDisabled}/>
+
      <Display initialLat={this.state.initialLat} initialLng={this.state.initialLng} town={this.state.town} county={this.state.county} />
     </>
     )
