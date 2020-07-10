10553690ba7ac7418e34ef08da79819211c2cdce
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-07-10 14:10:56 -0400

    Added quit button functionality.

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -5,13 +5,14 @@ import React from "react"
 class Button extends React.Component{
 
 
+
 //Renders the page. Creates buttons 
     render(){
         return ( 
        <div>
-           <button className="button" id="startButton" onClick= {this.props.handleStart}> Start Game</button> {/* Create Start Button */}
-           <button disabled={true} className="button" id= "guessButton" onClick= {this.props.handleGuess}> Guess</button> {/* Create Guess Button */}
-           <button disabled={true} className="button" id= "quitButton" onClick= {this.props.handleQuit}> Quit</button> {/* Create Quit Button */}
+           <button className="button" id="startButton" disabled={this.props.startDisabled} onClick= {this.props.handleStart}> Start Game</button> {/* Create Start Button */}
+           <button disabled={true} className="button" disabled={this.props.guessDisabled} id= "guessButton" onClick= {this.props.handleGuess}> Guess</button> {/* Create Guess Button */}
+           <button disabled={true} className="button" disabled={this.props.quitDisabled} id= "quitButton" onClick= {this.props.handleQuit}> Quit</button> {/* Create Quit Button */}
        </div> 
     )}
 }
--- src/index.js
+++ src/index.js
@@ -1,101 +1,129 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import VTMap from './Components/VTMap.js'
-import Button from "./Components/Buttons.js"
-import Display from "./Components/InfoBox.js"
-import borderData from "./Components/border.js"
-import leafletPip from 'leaflet-pip'
-import L from 'leaflet'
-import {Map, Marker, TileLayer, Polygon} from 'react-leaflet'
+import React from "react";
+import ReactDOM from "react-dom";
+import VTMap from "./Components/VTMap.js";
+import Button from "./Components/Buttons.js";
+import Display from "./Components/InfoBox.js";
+import borderData from "./Components/border.js";
+import leafletPip from "leaflet-pip";
+import L from "leaflet";
+import { Map, Marker, TileLayer, Polygon } from "react-leaflet";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      initialLat: 43.900,
+      initialLat: 43.9,
       initialLng: -72.7317,
-      currentLat: 43.900,
+      currentLat: 43.9,
       currentLng: -72.7317,
-      town: '?',
-      county: '?',
+      town: "?",
+      county: "?",
       latDisplay: "?",
       lngDisplay: "?",
-      startEnabled: false,
-      guessDisabled: true, 
-      quitDisabled: true, 
-      zoom: 8
-    }
+      startDisabled: false,
+      guessDisabled: true,
+      quitDisabled: true,
+      zoom: 8,
+    };
   }
 
   //When user clicks on start, the start button is disabled and the guess and quit button are enabled
-  handleStart= (evt)=>{
-        
+  handleStart = (evt) => {
+    
+
     evt.preventDefault();
-    this.setState({
-        startEnabled: true,
-        guessDisabled: false,
-        quitDisabled: false
-    })
-    //Declare Variables 
-    let startButton = document.getElementById("startButton");
-    let guessButton = document.getElementById("guessButton");
-    let quitButton = document.getElementById("quitButton");
-
-    //Disables the start button and enable the guess and quit button
-    startButton.disabled =true;
-    guessButton.disabled =false;
-    quitButton.disabled= false;
-
-    //Creates the array of location lat and lng
     let start = randomLocation();
     this.setState({
+      startDisabled: true,
+      guessDisabled: false,
+      quitDisabled: false,
       currentLat: start[1],
       currentLng: start[0],
       initialLat: start[1],
       initialLng: start[0],
-      zoom: 18
-    })
+      zoom: 18,
+    });
 
 
-  //Gives a random lat and lng between the max and min lat and lng of Vermont. 
-    function randomLocation() { 
-    let randomLng = -1 * (Math.random() * (71.510225 - 73.352182 ) +73.352182)
-    let randomLat = (Math.random()*( 45.005419-42.730315)+42.730315 );
-  
-    let stateBorder = L.geoJson(borderData)
-    console.log(stateBorder)
+    //Gives a random lat and lng between the max and min lat and lng of Vermont.
+    function randomLocation() {
+      let randomLng =
+        -1 * (Math.random() * (71.510225 - 73.352182) + 73.352182);
+      let randomLat = Math.random() * (45.005419 - 42.730315) + 42.730315;
 
-    let results = leafletPip.pointInLayer([randomLng, randomLat], stateBorder)
-    console.log(results)
-  
-    while(results.length === 0) {
-     randomLng = -1 * (Math.random() * (71.510225 - 73.352182 ) +73.352182)
-     randomLat = (Math.random()*( 45.005419-42.730315)+42.730315 );
-     results = leafletPip.pointInLayer([randomLng, randomLat], stateBorder)
-    }
-  
-    return [randomLng, randomLat]
+      let stateBorder = L.geoJson(borderData);
+      
 
+      let results = leafletPip.pointInLayer(
+        [randomLng, randomLat],
+        stateBorder
+      );
    
-  }
- }
+
+      while (results.length === 0) {
+        randomLng = -1 * (Math.random() * (71.510225 - 73.352182) + 73.352182);
+        randomLat = Math.random() * (45.005419 - 42.730315) + 42.730315;
+        results = leafletPip.pointInLayer([randomLng, randomLat], stateBorder);
+      }
+
+      return [randomLng, randomLat];
+    }
+  };
+
+  //Function handles a quit, displays the lat/long, town and county
+  handleQuit = (evt) => {
+     
+
+    evt.preventDefault();
+   
+    //Sets the state of the buttons
+    this.setState({
+      startDisabled: false,
+      guessDisabled: true,
+      quitDisabled: true,
+      town: "?",
+      county: "?",
+      latDisplay: this.state.initialLat,
+      lngDisplay: this.state.initialLng,
+    });
+  };
 
 
-
-render() {
-    console.log(this.state)
+  render() {
+    console.log(this.state);
     return (
-    <>
-     <VTMap currentLat={this.state.initialLat} currentLng={this.state.initialLng} zoomFactor= {this.state.zoom}/>
+      <>
+        <VTMap
+          currentLat={this.state.initialLat}
+          currentLng={this.state.initialLng}
+          zoomFactor={this.state.zoom}
+        />
 
-     <Button handleStart= {this.handleStart} startEnabled ={this.startEnabled} guessDisabled= {this.guessDisabled}quitDisabled= {this.quitDisabled}/>
+        <Button
+        //Adds methods to the start function
+          handleStart={this.handleStart}
+          startDisabled={this.state.startDisabled}
 
-     <Display initialLat={this.state.initialLat} initialLng={this.state.initialLng} town={this.state.town} county={this.state.county} latDisplay={this.state.latDisplay} lngDisplay={this.state.lngDisplay}/>
-    </>
-    )
+          //Adds methods to the guess function
+          guessDisabled={this.state.guessDisabled}
+
+          //Adds methods to the quit button
+          quitDisabled={this.state.quitDisabled} 
+          handleQuit= {this.handleQuit}
+        />
+
+        <Display
+          initialLat={this.state.initialLat}
+          initialLng={this.state.initialLng}
+          town={this.state.town}
+          county={this.state.county}
+          latDisplay={this.state.latDisplay}
+          lngDisplay={this.state.lngDisplay}
+        />
+      </>
+    );
   }
-  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
+ReactDOM.render(<App />, document.getElementById("root"));
